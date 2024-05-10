05aad9b0993e295ab40b22dd527d8fcd0879aa40
Thayna <thayrcristina@gmail.com>
2024-05-09 23:05:18 -0300

    Tipos numericos

---
+++ 03.03-tipos-numericos/TiposNumericos.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TiposNumericos  {
+    public static void main(String[] args) {
+        Byte numeroByte = 127; // -127 a 127
+
+        Short numeroShort = -32768; // -32768 a 32767
+
+        Integer numeroInteger = 214783647; // -214783648 (2 elevado a 31) até 214783647
+
+        Long numeroLong = 1L; // -9,223,372,036,854,775,808 a 9,223,372,036,854,775,807
+
+        //Decimais
+
+        Float numeroFloat = 1.0f;
+
+        Double numeroDouble = 1.0;
+    }
+}
